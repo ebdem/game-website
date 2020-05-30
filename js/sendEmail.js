6bb2b1475892f7c9530fa77cdfe877a4d7cf0a0b
Ramazan Cetin <ramazancetinn@yandex.com>
2020-05-31 00:29:57 +0300

    Add sendEmail function

---
+++ js/sendEmail.js
@@ -0,0 +1,42 @@
+document.getElementById('sendButton').addEventListener('click', function() {
+    
+    const name = document.getElementById('nameInput').value
+    const surname = document.getElementById('surnameInput').value
+    const email = document.getElementById('emailInput').value
+    const message = document.getElementById('messageInput').value
+
+
+    if (!name || !surname || !email || !message){
+
+        // set background of the empty inputs to red
+    
+    }else{
+        const  postData = {
+            name,
+            surname,
+            email,
+            message
+        }
+    
+        // post credentials to send_email endpoint
+    
+        $.ajax({
+            url: 'http://localhost:3000/send_email',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(postData),
+            success: function(res){
+                console.log(res)
+
+                // show user a success m essage
+
+
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        })
+    }
+
+
+})
